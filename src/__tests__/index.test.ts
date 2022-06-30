cbd31757eb1b77c80da30d1c99fe129c610cc0cd
陶科 <taoke.com>
2022-06-30 23:08:03 +0800

    feat: feat: 添加初始化对象

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,16 @@
+// import SKCarousel from '../components/index'
+// const initializeEmbla = (
+//   withContainer: boolean = true,
+//   withSlides: boolean = true
+// ): (() => SKCarouselType) => {
+//   const rootNode = document.createElement('div')
+//   const containerNode = document.createElement('div')
+
+//   if (withContainer) rootNode.appendChild(containerNode)
+//   if (withSlides) {
+//     for (let i = 0; i < 3; i += 1) {
+//       containerNode.appendChild(document.createElement('div'))
+//     }
+//   }
+//   return () => SKCarousel(rootNode)
+// }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,25 @@
+// export type SKNodesType = {
+//   root: HTMLElement
+//   container?: HTMLElement
+//   slides?: HTMLElement[]
+// }
+
+// export type SKCarouselType = {}
+
+// function SKCarousel(nodes: HTMLElement | EmblaNodesType): SKCarouselType {
+//   let root: HTMLElement
+//   let container: HTMLElement
+//   let slides: HTMLElement[]
+
+//   function storeElements(): void {
+//     const providedContainer = 'container' in nodes && nodes.container
+//     const providedSlides = 'slides' in nodes && nodes.slides
+
+//     root = 'root' in nodes ? nodes.root : nodes
+//     container = providedContainer || <HTMLElement>root.children[0]
+//     slides = providedSlides || [].slice.call(container.children)
+//   }
+// }
+
+// export default SKCarousel
 export default {}
